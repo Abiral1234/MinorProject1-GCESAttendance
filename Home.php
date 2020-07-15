0dbadaedcf66fbd7e114a9df5156f05f78ae6f9e
Abiral1234 <abiral.pokhrel99@gmail.com>
2020-07-15 13:12:54 +0545

    Added table to the home page

--- Home.php
+++ Home.php
@@ -1,92 +1,59 @@
+<?php 
+include_once 'connection.php';  ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
-	<style type="text/css">
-		body{
-			margin: 0;
-			background: white;
-			font-family: sans-serif;
-			font-weight: 350;
-		}
-		.container{
-			width: 80%;
-			margin: 0 auto;
-			
-		}
-		header{
-			background:#00BEFF;
-			}
-
-		header::after{
-			content: '';
-			display: table;
-			clear: both;
-		}
-
-		nav ul >li>a{
-			color: white;
-			text-underline-position: none;
-			text-decoration: none;
-			text-transform: uppercase;
-			font-size: 25px;
-
-		}
-		nav ul >li>a:hover{
-			color: white;
-			
-		}
-		nav{
-			float: right;
-		}
-		nav ul{
-			margin: 0;
-			padding: 0;
-			list-style: none;
-		}
-		nav li{
-			display: inline-block; 
-			margin-left: 70px;
-			padding-top: 25px;
-			padding-bottom: 25px;
-			position: relative;
-		}
-		
-		nav a::before{
-			content: '';
-			display: block;
-			height: 5px;
-			width: 100%;
-			background-color: white;
-			position: absolute;
-			top :0 ;
-			width: 0% ;
-			transition: all ease-in-out 250ms;
-			font-style: bold;
-
-		}
-		nav a:hover::before{
-			width: 100%;
-
-		}
-
-
-	</style>
+	<link rel="stylesheet" type="text/css" href="CSS/Homestyle.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
 </head>
 <body>
 	<header>
-<div class="container">	
+<div class="navigation">	
 	<nav>
-
-
 		<ul>
-			<li><a href="#">Home</a> </li>
-			<li><a href="#">View</a> </li>
+			<li><a href="Home.php">Home</a> </li>
+			<li><a href="view.php">View</a> </li>
 			<li><a href="#">Result</a> </li>
-			<li><a href="#">logout</a> </li>
+			<li><a href="logout.php">logout</a> </li>
 		</ul>
 	</nav>
-
+</div>
+<div class="container" >
+	<h1>Attendance<h1>
+</div>
+<div class="pannel">
+	<a href="#" id="viewbtn">View</a>
+	<a href="#" id="addstud_btn">Add student</a><br><br><br>
+	<table class="table1">
+		<thead>
+			<tr>
+				<th>Roll no    </th>
+				<th>Name       </th>
+				<th>Year       </th>
+				<th>Subject    </th>
+				<th>Attendance</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+				$sql = " SELECT * FROM `add student` " ;
+				$result = mysqli_query($conn ,$sql);
+				$row = mysqli_fetch_assoc($result)
+						
+			?>
+			<tr>
+				<td><?php echo $row['Roll no']; ?></td>
+				<td><?php echo $row['name']; ?></td>
+				<td><?php echo $row['year']; ?></td>
+				<td><?php echo $row['subject']; ?></td>
+				<td>
+					<input type="radio" name="attendance" value="present">Present
+					<input type="radio" name="attendance" value="absent">Absent
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
 </header>
 </body>
---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'gces attendance');
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+//Check the connection
+if($conn == false){
+    dir('Error: Cannot connect');
+}
+
+?>
