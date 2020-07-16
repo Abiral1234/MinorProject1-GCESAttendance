ac3594672d25fcc1b2e7f887ad4de5f049661e95
milankshatri17@gmail.com <milankshatri17@gmail.com>
2020-07-16 14:23:00 +0545

    Batch selection page

---
+++ BatchSel.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Css/BSstyle.css">
+    <title>Select the Batch</title>
+</head>
+<body>
+    <img src="Images/addstd.jpg" class="img">
+    <div class="container">
+        <div class="heading">
+            <h3>Select the Batch</h3>
+        </div>
+        <form action="">
+            <div class="content">
+                <div class="be">
+                <h3>Bachelor of Software Engineering</h3>
+                    <div class="bese">
+                        <input type="submit" value="BESE2016" class="btn" formaction="BESE2016.php"><br>
+                        <input type="submit" value="BESE2017" class="btn" formaction="BESE2017.php"><br>
+                        <input type="submit" value="BESE2019" class="btn" formaction="BESE2018.php"><br>
+                        <input type="submit" value="BESE2019" class="btn" formaction="BESE2019.php"><br>
+                    </div>
+                </div>
+                <div class="be">
+                <h3>Bachelor of Computer Engineering</h3>
+                    <div class="bece">
+                        <input type="submit" value="BECE2016" class="btn" formaction="BECE2016.php"><br>
+                        <input type="submit" value="BECE2017" class="btn" formaction="BECE2017.php"><br>
+                        <input type="submit" value="BECE2018" class="btn" formaction="BECE2018.php"><br>
+                        <input type="submit" value="BECE2018" class="btn" formaction="BECE2019.php"><br>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
