6870f74d6fd1211aa58cf83df60d118061780ece
milankshatri17@gmail.com <milankshatri17@gmail.com>
2020-07-16 14:31:24 +0545

    changes on BatchSel.php

--- BatchSel.php
+++ BatchSel.php
@@ -12,28 +12,26 @@
         <div class="heading">
             <h3>Select the Batch</h3>
         </div>
-        <form action="">
             <div class="content">
                 <div class="be">
                 <h3>Bachelor of Software Engineering</h3>
                     <div class="bese">
-                        <input type="submit" value="BESE2016" class="btn" formaction="BESE2016.php"><br>
-                        <input type="submit" value="BESE2017" class="btn" formaction="BESE2017.php"><br>
-                        <input type="submit" value="BESE2019" class="btn" formaction="BESE2018.php"><br>
-                        <input type="submit" value="BESE2019" class="btn" formaction="BESE2019.php"><br>
+                        <input type="button" value="BESE2016" class="btn" onclick="location.href='BESE2016.php';"><br>
+                        <input type="button" value="BESE2017" class="btn" onclick="location.href='BESE2017.php';"><br>
+                        <input type="button" value="BESE2019" class="btn" onclick="location.href='BESE2018.php';"><br>
+                        <input type="button" value="BESE2019" class="btn" onclick="location.href='BESE2019.php';"><br>
                     </div>
                 </div>
                 <div class="be">
                 <h3>Bachelor of Computer Engineering</h3>
                     <div class="bece">
-                        <input type="submit" value="BECE2016" class="btn" formaction="BECE2016.php"><br>
-                        <input type="submit" value="BECE2017" class="btn" formaction="BECE2017.php"><br>
-                        <input type="submit" value="BECE2018" class="btn" formaction="BECE2018.php"><br>
-                        <input type="submit" value="BECE2018" class="btn" formaction="BECE2019.php"><br>
+                        <input type="button" value="BECE2016" class="btn" onclick="location.href='BECE2016.php';"><br>
+                        <input type="button" value="BECE2017" class="btn" onclick="location.href='BECE2017.php';"><br>
+                        <input type="button" value="BECE2018" class="btn" onclick="location.href='BECE2018.php';"><br>
+                        <input type="button" value="BECE2018" class="btn" onclick="location.href='BECE2019.php';"><br>
                     </div>
                 </div>
             </div>
-        </form>
     </div>
 </body>
 </html>
