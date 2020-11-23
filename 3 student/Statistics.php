ede78507c01ca2bc36998df471a010bfacf5ab45
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2020-11-23 12:44:52 +0545

    change in css

--- 3 student/Statistics.php
+++ 3 student/Statistics.php
@@ -75,7 +75,7 @@
         s2.options.add(newOption);
 
         }
-        }
+      }
   </script> 
 </head>
 
@@ -83,7 +83,7 @@
 <div class="background_image"></div>
 <div class="background_image2"></div>
 
-<!-- <header>
+ <header>
       <nav class="navbar">
         <div class="brand-title">Gces Attendance</div>
           <a href="#" class="toggle-button">
@@ -96,14 +96,14 @@
             <li><a href="Home.php">Home</a> </li>
             <li><a href="view.php">View</a> </li>
             <li><a href="Statistics.php">Statistics</a> </li>     			   <!-- nav bar -->
-            <!-- <li><a href="notice.php">Notice</a></li>
+             <li><a href="notice.php">Notice</a></li>
             <li><a href="../index.php">logout</a> </li>
         </ul>
         </div>
 	  </nav>
    
 
-</header> -->
+</header>
 
 <main>
         <section>
