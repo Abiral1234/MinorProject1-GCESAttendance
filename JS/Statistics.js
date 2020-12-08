9ef2c9d0937a1d66147235ac4a0485fa0ce71492
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2020-12-08 16:17:12 +0545

    Fixed errors

--- JS/Statistics.js
+++ JS/Statistics.js
@@ -1,13 +1,17 @@
 function searchFunction() {
+
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("userInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("table1");
     tr = table.getElementsByTagName("tr");
+
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[1];
+
       if (td) {
         txtValue = td.textContent || td.innerText;
+        
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
         } else {
@@ -21,29 +25,50 @@ function searchFunction() {
 function sortTable(n) {
   
   var table, rows, switching, i, x, y, shouldSwitch, dir, 
-  switchcount = 0;table = document.getElementById("table1");
+  switchcount = 0;
+  table = document.getElementById("table1");
   switching = true;
   dir = "asc"; 
 
   while (switching) {
     switching = false;
-    rows = table.getElementsByTagName("TR");
+    rows = table.getElementsByTagName("tr");
 
     for (i = 1; i < (rows.length - 1); i++) {
       shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
- 
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          shouldSwitch= true;
-          break;
-        }
-      } else if (dir == "desc") {
+      x = rows[i].getElementsByTagName("td")[n].innerHTML;
+      y = rows[i + 1].getElementsByTagName("td")[n].innerHTML;
 
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          shouldSwitch= true;
-          break;
+      if(n!=1){
+        x = parseInt(x);
+        y = parseInt(y);
+
+        if (dir == "asc") {
+
+          if (x > y) {
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+  
+          if (x < y) {
+            shouldSwitch= true;
+            break;
+          }
+        }
+      }else{
+        if (dir == "asc") {
+
+          if (x.toLowerCase() > y.toLowerCase()) {
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+  
+          if (x.toLowerCase() < y.toLowerCase()) {
+            shouldSwitch= true;
+            break;
+          }
         }
       }
     }
