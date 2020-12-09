f1bf4e27e32506ade4578698689bb24764e26db3
Abiral1234 <abiral.pokhrel99@gmail.com>
2020-12-09 09:49:27 +0545

    Database connection to student homepage

--- 2 teacher/home.php
+++ 2 teacher/home.php
@@ -122,7 +122,7 @@ NoAttendance:
 <head>
 	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="../CSS/homecss13.css">
+	<link rel="stylesheet" type="text/css" href="../CSS/homecss15.css">
 	<link rel="stylesheet" type="text/css" href="../CSS/image2.css">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
 
