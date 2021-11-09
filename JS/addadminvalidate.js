7d47da53158db846f6c867deb3b09326fc0f5e66
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2021-11-09 18:58:25 +0545

    Validation in addAdmin page

---
+++ JS/addadminvalidate.js
@@ -0,0 +1,18 @@
+document.form1.addEventListener( "submit", function(event){
+    var user = document.getElementById("username").value;
+    var pwd = document.getElementById("pword").value;
+    if(user == "" || pwd == "" || pwd.length<6){
+        text = "Invalid Username or Password";
+        event.preventDefault();
+    }
+    var invalid = document.getElementById("invalid");
+    invalid.innerHTML = text;
+    invalid.style.backgroundColor = '#FAB8B8';
+    invalid.style.borderRadius = '5px';
+    invalid.style.width = '40%'
+    invalid.style.padding = '10px';
+    invalid.style.color = '#DA1313';
+    invalid.style.border = '2px solid #DA1313'
+    invalid.style.fontSize = '1.2rem';
+    invalid.style.fontFamily = 'sans-serif';
+})
